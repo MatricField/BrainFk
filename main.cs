db68055ee05a13ab54e9e312e242116778f773f5
Mingxi "Lucien <david.xelnaga@gmail.com>
2019-12-26 01:10:35 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,17 @@
+using System;
+
+sealed class InfiniArray<T>
+{
+  private const long SEG_LEN=2048;
+  
+}
+
+abstract class MachineModel
+{
+}
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hello World");
+  }
+}
