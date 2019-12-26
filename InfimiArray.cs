a98e92b83bd1f52dc9ffea4075b1061ec960811d
Mingxi "Lucien" Du <david.xelnaga@gmail.com>
2019-12-26 01:48:31 +0000

    half bake array

---
+++ InfimiArray.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BrainFuck
+{
+  internal sealed class InfiniArray<T>
+  {
+    private const int SEG_LEN = 2048;
+    private List<List<T>> data = new List<List<T>>();
+    
+    private void EnsureIndex(long index)
+    {
+      var seg = Convert.ToInt32(index/SEG_LEN);
+      var diff = Convert.ToInt32(index%SEG_LEN);
+      if(seg >= data.Count)
+      {
+        data.AddRange(Enumerable.Repeat<List<T>>(null, seg - data.Count + 1));
+      }
+      if(null == data[seg])
+      {
+        data[seg] = new List<T>();
+      }
+      if(diff >= data[seg].Count)
+      {
+        data[seg].AddRange(Enumerable.Repeat(default(T), diff - data[seg].Count + 1));
+      }
+    }
+    
+    public T this[long i]
+  }
+}
--- main.cs
+++ main.cs
@@ -1,15 +1,5 @@
 using System;
 
-sealed class InfiniArray<T>
-{
-  private const long SEG_LEN=2048;
-  
-}
-
-abstract class MachineModel
-{
-}
-
 class MainClass {
   public static void Main (string[] args) {
     Console.WriteLine ("Hello World");
